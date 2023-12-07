65e68a6781c17381316e3353b44aabd4e790e1b4
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2023-12-07 23:49:48 +0500

    first commit

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import requests
 
 city = input("Enter the name of the city\n")
 url = "f"
-#In sha Allah, I will do my best in interview! and crack the 
+#In sha Allah, I will do my best in interview! and crack 
