d3c64eb11c8d3e0caeea3a024e0b720c641f87ef
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2023-12-08 23:42:33 +0500

    first commit

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import requests
 
 city = input("Enter the name of the city\n")
 url = "f"
-#I have to kill my wakness and do attention to focus in detail and 
+#I have to kill my wakness and do attention to focus in 
