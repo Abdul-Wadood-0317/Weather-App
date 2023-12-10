81c296ace9165da6c664505a9decb71c21ca16b2
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2023-12-11 01:49:06 +0500

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import requests
+import json
+import os
+import win32com.client as wincom
 
 city = input("Enter the name of the city\n")
-url = "f"
+url = f"https://api.weatherapi.com/v1/current.json?key=19b456ca3a4c4321acf205911230612&q={city}"
+r = requests.get(url)
+# print(r.text)
+weatherdic = json.loads(r.text)
+w = weatherdic["current"]["temp_c"]
+
+os.system(f"say'The current waether in {city} is {w} degrees.'")
