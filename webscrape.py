4056dd05d8b32cd3672e063e1348fbc4d2864772
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2023-12-16 23:57:35 +0500

    first commit

--- webscrape.py
+++ webscrape.py
@@ -27,4 +27,4 @@ if response.status_code == 200:
     print("\n".join(urdu_text))
 else:
     print(f"Error: Could not access the URL. Status code: {response.status_code}")
-#Today i learnt two lessons ,Consistency and Balance in 
+#Today i learnt two lessons ,Consistency and 
